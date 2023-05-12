d9ca2a9210a3235bf95dfdc68f7917401d25c6d1
AlexandreChavesDias <43960331+AlexandreChavesDias@users.noreply.github.com>
2023-05-12 14:29:43 -0300

    Add files via upload

---
+++ ApiHexagonal/src/main/java/com/example/hexagonal/ApiHexagonal/ApiHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal.ApiHexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHexagonalApplication.class, args);
+	}
+
+}
